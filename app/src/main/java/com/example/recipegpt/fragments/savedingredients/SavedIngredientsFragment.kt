a14f26e34981b515d7ea7baf85fd0063b3ddfb69
mmswflow-upb <mmswflow@gmail.com>
2024-12-25 08:02:11 +0200

    Added Saved Ingredients Fragment

---
+++ app/src/main/java/com/example/recipegpt/fragments/savedingredients/SavedIngredientsFragment.kt
@@ -0,0 +1,13 @@
+package com.example.recipegpt.fragments.savedingredients
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.recipegpt.R
+
+class SavedIngredientsFragment : Fragment(R.layout.fragment_saved_recipes) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Your fragment logic
+    }
+}
---
+++ app/src/main/java/com/example/recipegpt/fragments/savedingredients/SavedIngredientsViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.recipegpt.fragments.savedingredients
+
+class SavedIngredientsViewModel {
+}
